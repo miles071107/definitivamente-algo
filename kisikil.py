f07297106d57c95d04596f98620865c6b82121e1
miles071107 <131699513+miles071107@users.noreply.github.com>
2024-03-07 18:02:28 +0000

    changes?

--- kisikil.py
+++ kisikil.py
@@ -1,3 +1,23 @@
-x = float(input('Digite o seu salário base: '))
-y = float(x - x*0.07 + x*0.05)
-print('seu salário é: ', y)
+#1)
+#x = float(input('Digite o seu salário base: '))
+#y = float(x - x*0.07 + x*0.05)
+#print('seu salário é: ', y)
+
+#2)
+#x = float(input('Digite o Valor do seu depósito: '))
+#y = float(input('Digite a taxa de juros anual: '))
+#y = y/100
+#print('seu rendimento: ', x*y)
+#print('valor total: ', x + x*y)
+
+#3)
+#x = float(input('Digite um valor em pés: '))
+#print('polegadas: ', x*12)
+#print('jardas: ', x/3)
+#print('milhas: ', (x/3)/1760)
+
+#4)
+#x = float(input('Digite um valor para depositar: '))
+#y = float(input('Digite um valor para retirar: '))
+#z = float(input('Digite mais um: '))
+#print('saldo final: ', (x-y-z)+(x*0.0038 + ((x-y)*0.0038)))
